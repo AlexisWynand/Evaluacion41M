dad8f0e9a277a062ea3b6d180b1d517280adda67
AlexisWynand <81430319+AlexisWynand@users.noreply.github.com>
2021-04-12 16:23:06 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,19 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBciwIARlztoVBqvc9yk_Gd1crjKuqjJaA",
+    authDomain: "evaluacion41-maxel.firebaseapp.com",
+    databaseURL: "https://evaluacion41-maxel-default-rtdb.firebaseio.com",
+    projectId: "evaluacion41-maxel",
+    storageBucket: "evaluacion41-maxel.appspot.com",
+    messagingSenderId: "308005708831",
+    appId: "1:308005708831:web:1d5b0211f8323f3041612a",
+    measurementId: "G-GV9MEJPJYP"
+});
